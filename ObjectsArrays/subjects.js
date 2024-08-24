3c2f29a605dad05b1747f6ad19fd750d4f00ba30
luyosamson <luyosamson@gmail.com>
2024-08-24 20:06:11 +0300

    splice function

---
+++ ObjectsArrays/subjects.js
@@ -0,0 +1,6 @@
+const subjects=['English','Kiswahili','Mathematics','Biology']
+const newSub=subjects.slice(2)
+console.log(newSub)
+const school='Multimedia'
+const newSchool=school.slice(1,3)
+console.log(newSchool)
