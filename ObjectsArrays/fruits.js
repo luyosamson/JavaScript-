d59798ef8a7d87ba8b31734638e93123f41831a4
luyosamson <luyosamson@gmail.com>
2024-08-24 17:51:29 +0300

    Concatenate

--- ObjectsArrays/fruits.js
+++ ObjectsArrays/fruits.js
@@ -1,4 +1,5 @@
 const fruits=['Apple','Guava','Avocado','PawPaw','Watermelon','Guava']
+const veg=['Cabbage','Kales','Tommatoes']
 
 const newCase=fruits.map((fruit)=>{
     return fruit.toUpperCase()
@@ -13,3 +14,6 @@ console.log(fruitLength)
 const message=fruits.map((fruit)=>{
     console.log(`I like ${fruit}`)
 })
+
+const newArr=fruits.concat(veg)
+console.log(newArr)
